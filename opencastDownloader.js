532c2564b0abbd4240eafc5aec98ab736dc5fc88
Dennis Eigner <deathkid@deigner.eu>
2019-08-22 13:54:45 +0200

    Initial Commit

---
+++ opencastDownloader.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Opencast Downloader
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Download Videos from Opencast TU Wien
+// @author       Dennis Eigner
+// @match        https://oc-presentation.ltcc.tuwien.ac.at/engage/theodul/ui/core.html?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    setTimeout(createButton, 5000);
+})();
+
+function createButton() {
+    // stuff you want to happen right away
+    console.log('Hello World!')
+    const menu = document.getElementById('menu'),
+          downloadButton = document.createElement('button');
+    downloadButton.innerHTML = 'Download';
+    downloadButton.onclick = function() {
+        const video = document.querySelector('video');
+        const videoUrl = video.getAttribute("src");
+        window.open(videoUrl);
+    };
+
+    menu.insertBefore(downloadButton, menu.firstChild);
+}
